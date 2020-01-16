251492020793149cedc4518ee656e49d84c77d36
chuck47286 <chuck472861399@163.com>
2020-01-16 21:01:59 +0000

    ${TIME} ${DATE} from the dell laptop.

--- Test/Worksheet1Test.java
+++ Test/Worksheet1Test.java
@@ -1,8 +1,11 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Rule;
 import org.junit.jupiter.api.*;
+import org.junit.rules.ExpectedException;
 
 
-/** @author 
+/** @author
  *  YuCheng
  * This class contains the test cases for Worksheet1 solutions.
  *  <WRITE YOUR TEST CASES BELOW>
@@ -10,15 +13,145 @@ import org.junit.jupiter.api.*;
 
 public class Worksheet1Test {
     Worksheet1 worksheet1;
-	@BeforeEach
+    @BeforeEach
     void setUp() {
         worksheet1= new Worksheet1();
     }
     @Test
     void power() {
-        int expected = 880;
-//        worksheet1.power();
-//        Assertions.assertEquals(expected, aeroplane1.getPassengerNumber());
+        int m = 3;
+        int n = 4;
+        int acres = worksheet1.power(m, n);
+        int exres = 81;
+        Assertions.assertEquals(exres, acres);
+    }
+    @Test
+    void fastPower() {
+        int m = 3;
+        int n = 4;
+        int acres = worksheet1.fastPower(m, n);
+        int exres = 81;
+        Assertions.assertEquals(exres, acres);
+    }
+    @Test
+    void negateAll() {
+        List list = new List(2, new List(
+                -5, new List(8,
+                new List(0, new List()))));
+        List acres = worksheet1.negateAll(list);
+//        worksheet1.printList(acres);
+        List exres = new List(-2, new List(
+                5, new List(-8,
+                new List(0, new List()))));
+        Assertions.assertEquals(exres, acres);
+    }
+    @Test
+    void find1() {
+        List list = new List(7, new List(
+                5, new List(3,
+                new List(8, new List()))));
+        int acres1 = worksheet1.find(3, list);
+//        worksheet1.printList(acres);
+        int exres1 = 2;
+        Assertions.assertEquals(exres1, acres1);
+    }
+    @Rule
+    public ExpectedException thrown= ExpectedException.none();
+    @Test
+    void find2() {
+        List list = new List(7, new List(
+                5, new List(3,
+                new List(8, new List()))));
+        try {
+            int acres2  = worksheet1.find(2, list);
+            int exres2 = -1;
+            Assertions.assertEquals(exres2, acres2);
+        } catch (IllegalArgumentException e) {
+            Assertions.assertEquals("cannot find", e.getMessage());
+        }
+    }
+    @Test
+    void allEven1() {
+        List list = new List(7, new List(
+                5, new List(3,
+                new List(8, new List()))));
+        boolean acres1  = worksheet1.allEven(list);
+        boolean exres1 = false;
+        Assertions.assertEquals(exres1, acres1);
+    }
+    @Test
+    void allEven2() {
+        List list = new List(6, new List(
+                0, new List(4,
+                new List(-8, new List()))));
+        boolean acres2  = worksheet1.allEven(list);
+        boolean exres2 = true;
+        Assertions.assertEquals(exres2, acres2);
+    }
+    @Test
+    void evenNumbers1() {
+        List list = new List(2, new List(
+                -5, new List(3,
+                new List(8, new List(-2, new List())))));
+        List acres1  = worksheet1.evenNumbers(list);
+        List exres1 = new List(2, new List(8, new List(-2, new List())));
+        Assertions.assertEquals(exres1, acres1);
+    }
+    @Test
+    void sorted1() {
+        List list = new List(2, new List(
+                -5, new List(3,
+                new List(8, new List(-2, new List())))));
+        boolean acres1  = worksheet1.sorted(list);
+        boolean exres1 = false;
+        Assertions.assertEquals(exres1, acres1);
+    }
+    @Test
+    void sorted2() {
+        List list = new List(-2, new List());
+        boolean acres2  = worksheet1.sorted(list);
+        boolean exres2 = true;
+        Assertions.assertEquals(exres2, acres2);
+    }
+    @Test
+    void sorted3() {
+        List list = new List(-2, new List(-2, new List()));
+        boolean acres3  = worksheet1.sorted(list);
+        boolean exres3 = true;
+        Assertions.assertEquals(exres3, acres3);
+    }
+    @Test
+    void merge1() {
+        List lista = new List(10, new List(-2, new List()));
+        List listb = new List(8, new List());
+        List acres1  = worksheet1.merge(lista, listb);
+        List exres1 = new List(10, new List(8, new List(-2, new List())));
+        Assertions.assertEquals(exres1, acres1);
+    }
+    @Test
+    void merge2() {
+        List lista = new List();
+        List listb = new List();
+        List acres2  = worksheet1.merge(lista, listb);
+        List exres2 = new List();
+        Assertions.assertEquals(exres2, acres2);
+    }
+    @Test
+    void removeDuplicates1() {
+        List lista = new List(9, new List(8, new List(8, new List(7,
+                new List(5, new List(5, new List(5, new List(2, new List()))))))));
+        List acres1  = worksheet1.removeDuplicates(lista);
+        List exres1 = new List(9, new List(8, new List(7,
+                 new List(5, new List(2, new List())))));
+        Assertions.assertEquals(exres1, acres1);
+    }
+    @Test
+    void removeDuplicates2() {
+        List lista = new List(8, new List(8, new List(8,
+                        new List(8, new List()))));
+        List acres2  = worksheet1.removeDuplicates(lista);
+        List exres2 = new List(8, new List());
+        Assertions.assertEquals(exres2, acres2);
     }
 }
 
--- src/Worksheet1.java
+++ src/Worksheet1.java
@@ -3,7 +3,15 @@
  */
 
  public class Worksheet1 {
-
+	// print list
+	static void printList(List<Integer> L) {
+		if (L.isEmpty()) {
+			return;
+		} else {
+			System.out.println(L.getHead());
+			printList(L.getTail());
+		}
+	}
 	// Exercise 1
 
 	 static int power(int m, int n) {
@@ -12,52 +20,89 @@
 	}
 
 	 static int fastPower(int m, int n) {
-
-	 	return 0; // replace this by your code
+		if (n == 0) return 1;
+		if (n == 1) return m;
+		if (n % 2 == 0)
+			return fastPower(m * m, n / 2);
+		else
+	 	return fastPower(m * m, n / 2) * m; // replace this by your code
 	}
 
 	// Exercise 2
 
 	 static List<Integer> negateAll(List<Integer> a) {
-		List<Integer> res = new List<>();
-//		 Integer head = a.getHead();
-		 return res; // replace this by your code
+//		List<Integer> res = new List<>();
+//		Integer aHead = a.getHead();
+		if (a.isEmpty()) {
+			return a;
+		} else {
+			return new List<>(a.getHead() * (-1), negateAll(a.getTail()));
+		}
+//		return res; // replace this by your code
 	}
 
 	// Exercise 3
-
 	 static int find(int x, List<Integer> a) {
-		return 0; // replace this by your code
+		if (a.isEmpty()) {
+			throw new IllegalArgumentException("cannot find");
+		} else {
+			if (x != a.getHead()) {
+				return find(x, a.getTail()) + 1;
+			} else
+			return 0;
+		}
 	}
 
 	// Exercise 4
 
 	 static boolean allEven(List<Integer> a) {
-		return true; // replace this by your code
+		if (a.isEmpty())
+			return true;
+		return a.getHead() % 2 == 0 && allEven(a.getTail()); // replace this by your code
 	}
 
 	// Exercise 5
 
 	 static List<Integer> evenNumbers(List<Integer> a) {
-		return new List<Integer>(); // replace this by your code
+		if (a.isEmpty())
+			return a;
+
+		return a.getHead() % 2 == 0 ?
+				new List<Integer>(a.getHead(), evenNumbers(a.getTail())):
+				evenNumbers(a.getTail()); // replace this by your code
 	}
 
 	// Exercise 6
 
 	 static boolean sorted(List<Integer> a) {
-		return true; // replace this by your code
+		if (a.isEmpty() || a.getTail().isEmpty()) return true;
+		return a.getHead() >= a.getTail().getHead() && sorted(a.getTail()); // replace this by your code
 	}
 
 	// Exercise 7
 
 	 static List<Integer> merge(List<Integer> a, List<Integer> b) {
-		return new List<Integer>(); // replace this by your code
+		if (a.isEmpty()) return b;
+		if (b.isEmpty()) return a;
+		return a.getHead()>= b.getHead()?
+				new List<Integer>(a.getHead(), merge(a.getTail(), b)):
+				new List<>(b.getHead(), merge(a,b.getTail())); // replace this by your code
 	}
 
 	// Exercise 8
 
 	static List<Integer> removeDuplicates(List<Integer> a) {
-		return new List<Integer>(); // replace this by your code
+		if (a.isEmpty() || a.getTail().isEmpty()) return a;
+		List<Integer> lista = isNotDuplicate(a);
+		return new List<Integer>(lista.getHead(), removeDuplicates(lista.getTail())); // replace this by your code
+	}
+
+	private static List<Integer> isNotDuplicate(List<Integer> a) {
+		if (a.isEmpty() || a.getTail().isEmpty()) return a;
+		if (a.getHead() == a.getTail().getHead()) {
+			return isNotDuplicate(a.getTail());
+		} else
+			return a;
 	}
 
 }
