9b6d8763403b26a9ee260e9141605758ce5a6e83
chuck47286 <chuck472861399@163.com>
2020-01-24 13:21:36 +0000

    2020-01-24 from the dell laptop.

---
+++ src/Worksheet2.java
@@ -0,0 +1,148 @@
+import java.util.ArrayList;
+
+/**
+ * @author YuCheng
+ * This class contains the solution for Worksheet2
+ */
+
+public class Worksheet2 implements Worksheet2Interface {
+
+	// Exercise 1
+
+	static Tree<Integer> negateAll(Tree<Integer> t) {
+		if (t.isEmpty()) {
+			return new Tree<>();
+		}
+		Tree<Integer> tleft = new Tree<>();
+		Tree<Integer> tright = new Tree<>();
+		if (!t.left.isEmpty()) {
+			tleft = negateAll(t.left);
+		}
+		if (!t.right.isEmpty()) {
+			tright = negateAll(t.right);
+		}
+		return new Tree<>(-t.value, tleft, tright);
+	}
+
+	// Exercise 2
+
+	static boolean allEven(Tree<Integer> a) {
+		if (a.isEmpty()) {
+			return true;
+		}
+		return a.value % 2 == 0 && allEven(a.left) && allEven(a.right);
+		/*boolean al = false;
+		boolean ar = false;
+		if (!a.left.isEmpty()) {
+			al = allEven(a.left);
+		}
+		if (!a.right.isEmpty()) {
+			ar = allEven(a.right);
+		}
+		return a.value % 2 == 0 && al && ar;*/
+	}
+
+	// Exercise 3
+	static int depth(Tree<Integer> a, int x) {
+		try {
+			return find(a, x, 0);
+		} catch (IllegalArgumentException e) {
+			return -1;
+		}
+	}
+	static int find(Tree<Integer> a, int x, int dep) {
+		if (a.isEmpty()) return -1;
+		if (a.value == x) {
+			return dep;
+		}
+		return Math.max(find(a.left, x, dep + 1),
+		find(a.right, x, dep + 1));
+
+
+	}
+	// Exercise 4
+	static <E> List<E> preorder(Tree<E> a) {
+		if (a.isEmpty()) {
+			return new List<>();
+		}
+		List<E> ar = new List<>();
+		List<E> al = new List<>();
+		if (!a.right.isEmpty()) {
+			 ar = preorder(a.right);
+		}
+		if (!a.left.isEmpty()) {
+			 al = preorder(a.left);
+		}
+//		List<E> lr = merge(al, ar);
+//		List<E> res = new List(a.value, lr);
+		return new List<>(a.value, merge(al, ar));
+	}
+
+	/**
+	 *
+	 * @param al The list from the left subtree.
+	 * @param ar The list from the right subtree.
+	 * @param <E> The generic type of the List.
+	 * @return The merge list of two list from subtrees.
+	 */
+	private static <E> List<E> merge(List<E> al, List<E> ar) {
+		if (al.isEmpty()) return ar;
+		return new List<>(al.getHead(), merge(al.getTail(), ar));
+	}
+
+
+	// Exercise 5
+
+	static boolean isSearchTree(Tree<Integer> a) {
+		if (a.isEmpty() || a.left.isEmpty() && a.right.isEmpty()) return true;
+		if (!a.left.isEmpty() && a.value <= a.left.value) return false;
+		if (!a.right.isEmpty() && a.value >= a.right.value) return false;
+		return isSearchTree(a.left) && isSearchTree(a.right);
+	}
+
+	// Exercise 6
+
+	static void printDescending(Tree<Integer> a) {
+		if (a.isEmpty() ) {
+			return;
+		}
+		printDescending(a.right);
+		System.out.println(a.value);
+		printDescending(a.left);
+	}
+
+	// Exercise 7
+
+	static int max(Tree<Integer> a) {
+		if (a == null || a.isEmpty()) return 0;
+		if (a.right.isEmpty()) {
+			return a.value;
+		}
+//		if (a.right.isEmpty())
+//			return a.value;
+		return max(a.right);
+	}
+
+	// Exercise 8
+
+	static Tree<Integer> delete(Tree<Integer> a, int x) {
+		return new Tree<Integer>();
+	}
+
+	// Exercise 9
+	static <E> boolean isHeightBalanced(Tree<E> a) {
+		return true;
+	}
+
+	// Exercise 10
+
+	static Tree<Integer> insertHB(Tree<Integer> a, int x) {
+		return new Tree<Integer>();
+	}
+
+	static Tree<Integer> deleteHB(Tree<Integer> a, int x) {
+		return new Tree<Integer>();
+	}
+
+}
+
---
+++ src/Worksheet2Test.java
@@ -0,0 +1,318 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+
+/** @author 
+ *  YuCheng
+ * This class contains the test cases for Worksheet2 solutions.
+ *  <WRITE YOUR TEST CASES BELOW>
+ */
+
+public class Worksheet2Test {
+    Worksheet2 worksheet2;
+    @BeforeEach
+    void setUp() {
+        worksheet2 = new Worksheet2();
+    }
+
+    static Tree<Integer> buildTree(Integer[] arr, int index) {
+        if (arr == null || arr.length == 0 || index >= arr.length || arr[index] == null) {
+            return new Tree<>();
+        }
+        return new Tree<>(arr[index],
+                buildTree(arr, index * 2 + 1), buildTree(arr, index * 2 + 2));
+    }
+    static List<Integer> buildList(Integer[] arr) {
+        List<Integer> res = new List<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] != null) {
+                res = new List<>(arr[i], res);
+            }
+        }
+        return res;
+    }
+    static void printList(List<Integer> list) {
+        if (!list.isEmpty()) {
+            System.out.println(list.toString());
+        }
+    }
+    // ---------------------------negateAll---------------------
+
+    @Test
+    void negateAll1() {
+        /*Tree<Integer> t = new Tree<>(1, new Tree<>(2, new Tree<>(), new Tree<>()),
+                new Tree<>(3, new Tree<>(4, new Tree<>(), new Tree<>()), new Tree<>()));*/
+        Integer[] arr = {1, 2, 3, null, null, 4};
+        Tree<Integer> t  = buildTree(arr, 0);
+//        String format = Tree.format(t);
+//        System.out.println(format);
+        Tree<Integer> acres = worksheet2.negateAll(t);
+        Integer[] exarr = {-1, -2, -3, null, null, -4};
+        Tree<Integer> exres = buildTree(exarr, 0);
+        Assertions.assertEquals(exres, acres);
+    }
+    @Test
+    void negateAll2() {
+        Integer[] arr = {-9, 0, null, 9};
+        Tree<Integer> t  = buildTree(arr, 0);
+//        String format = Tree.format(t);
+//        System.out.println(format);
+        Tree<Integer> acres = worksheet2.negateAll(t);
+        Integer[] exarr = {9, 0, null, -9};
+        Tree<Integer> exres = buildTree(exarr, 0);
+        Assertions.assertEquals(exres, acres);
+    }
+    @Test
+    void negateAll3() {
+        Integer[] arr = null;
+        Tree<Integer> t  = buildTree(arr, 0);
+//        String format = Tree.format(t);
+//        System.out.println(format);
+        Tree<Integer> acres = worksheet2.negateAll(t);
+        Integer[] exarr = null;
+        Tree<Integer> exres = buildTree(exarr, 0);
+        Assertions.assertEquals(exres, acres);
+    }
+// ---------------------------allEven---------------------
+    @Test
+    void allEven1() {
+        /*Tree<Integer> t = new Tree<>(1, new Tree<>(2, new Tree<>(), new Tree<>()),
+                new Tree<>(3, new Tree<>(4, new Tree<>(), new Tree<>()), new Tree<>()));*/
+        Integer[] arr = {1, 2, 3, null, null, 4};
+        Tree<Integer> t  = buildTree(arr, 0);
+//        String format = Tree.format(t);
+//        System.out.println(format);
+        boolean acres = worksheet2.allEven(t);
+        boolean exres = false;
+        Assertions.assertEquals(exres, acres);
+    }
+    @Test
+    void allEven2() {
+        /*Tree<Integer> t = new Tree<>(1, new Tree<>(2, new Tree<>(), new Tree<>()),
+                new Tree<>(3, new Tree<>(4, new Tree<>(), new Tree<>()), new Tree<>()));*/
+        Integer[] arr = null;
+        Tree<Integer> t  = buildTree(arr, 0);
+//        String format = Tree.format(t);
+//        System.out.println(format);
+        boolean acres = worksheet2.allEven(t);
+        boolean exres = true;
+        Assertions.assertEquals(exres, acres);
+    }
+    @Test
+    void allEven3() {
+        /*Tree<Integer> t = new Tree<>(1, new Tree<>(2, new Tree<>(), new Tree<>()),
+                new Tree<>(3, new Tree<>(4, new Tree<>(), new Tree<>()), new Tree<>()));*/
+        Integer[] arr = {2, 4, null, 6, 8};
+        Tree<Integer> t  = buildTree(arr, 0);
+//        String format = Tree.format(t);
+//        System.out.println(format);
+        boolean acres = worksheet2.allEven(t);
+        boolean exres = true;
+        Assertions.assertEquals(exres, acres);
+    }
+// ---------------------------depth---------------------
+    @Test
+    void depth1() {
+        /*Tree<Integer> t = new Tree<>(1, new Tree<>(2, new Tree<>(), new Tree<>()),
+                new Tree<>(3, new Tree<>(4, new Tree<>(), new Tree<>()), new Tree<>()));*/
+        Integer[] arr = {5, 3, 8, 1, 4, 6, null};
+        Tree<Integer> t  = buildTree(arr, 0);
+//        String format = Tree.format(t);
+//        System.out.println(format);
+        int acres = worksheet2.depth(t,3);
+        int exres = 1;
+        Assertions.assertEquals(exres, acres);
+    }
+    @Test
+    void depth2() {
+//        Tree<Integer> t = new Tree<>(1, new Tree<>(2, new Tree<>(), new Tree<>()),
+//                new Tree<>(3, new Tree<>(4, new Tree<>(), new Tree<>()), new Tree<>()));
+        Integer[] arr = null;
+        Tree<Integer> t  = buildTree(arr, 0);
+//        String format = Tree.format(t);
+//        System.out.println(format);
+        int acres = worksheet2.depth(t,0);
+        int exres = -1;
+        Assertions.assertEquals(exres, acres);
+    }
+    @Test
+    void depth3() {
+//        Tree<Integer> t = new Tree<>(1, new Tree<>(2, new Tree<>(), new Tree<>()),
+//                new Tree<>(3, new Tree<>(4, new Tree<>(), new Tree<>()), new Tree<>()));
+        Integer[] arr = {2, 4, null, 6, 8};
+        Tree<Integer> t  = buildTree(arr, 0);
+//        String format = Tree.format(t);
+//        System.out.println(format);
+        int acres = worksheet2.depth(t,5);
+        int exres = -1;
+        Assertions.assertEquals(exres, acres);
+    }
+
+    // ---------------------------preorder---------------------
+    @Test
+    void preorder1() {
+        /*Tree<Integer> t = new Tree<>(1, new Tree<>(2, new Tree<>(), new Tree<>()),
+                new Tree<>(3, new Tree<>(4, new Tree<>(), new Tree<>()), new Tree<>()));*/
+        Integer[] arr = {5, 2, 6, 1, 4, null, 8, null, null, 3, null, null, null, 7, 9};
+        Tree<Integer> t  = buildTree(arr, 0);
+//        String format = Tree.format(t);
+//        System.out.println(format);
+        List<Integer> acres = worksheet2.preorder(t);
+//        printList(acres);
+        Integer[] exarr = {5, 2, 1, 4, 3, 6, 8, 7, 9};
+        List<Integer> exres = buildList(exarr);
+        Assertions.assertEquals(exres, acres);
+    }
+    @Test
+    void preorder2() {
+        /*Tree<Integer> t = new Tree<>(1, new Tree<>(2, new Tree<>(), new Tree<>()),
+                new Tree<>(3, new Tree<>(4, new Tree<>(), new Tree<>()), new Tree<>()));*/
+        Integer[] arr = {5, 1, 4};
+        Tree<Integer> t  = buildTree(arr, 0);
+//        String format = Tree.format(t);
+//        System.out.println(format);
+        List<Integer> acres = worksheet2.preorder(t);
+//        printList(acres);
+        Integer[] exarr = {5, 1, 4};
+        List<Integer> exres = buildList(exarr);
+        Assertions.assertEquals(exres, acres);
+    }
+    @Test
+    void preorder3() {
+        /*Tree<Integer> t = new Tree<>(1, new Tree<>(2, new Tree<>(), new Tree<>()),
+                new Tree<>(3, new Tree<>(4, new Tree<>(), new Tree<>()), new Tree<>()));*/
+//        Integer[] arr = {};
+        Integer[] arr = null;
+        Tree<Integer> t  = buildTree(arr, 0);
+//        String format = Tree.format(t);
+//        System.out.println(format);
+        List<Integer> acres = worksheet2.preorder(t);
+//        printList(acres);
+        List<Integer> exres = new List<>();
+        Assertions.assertEquals(exres, acres);
+    }
+    // ---------------------------isSearchTree---------------------
+    @Test
+    void isSearchTree1() {
+        Integer[] arr = null;
+        Tree<Integer> t  = buildTree(arr, 0);
+        boolean acres = worksheet2.isSearchTree(t);
+        boolean exres = true;
+        Assertions.assertEquals(acres, exres);
+    }
+    @Test
+    void isSearchTree2() {
+//        Integer[] arr = {5, 6, 2};
+        Integer[] arr = {5, 6, 6};
+//        Integer[] arr = {5, 2, 6};
+        Tree<Integer> t  = buildTree(arr, 0);
+        boolean acres = worksheet2.isSearchTree(t);
+        boolean exres = false;
+//        boolean exres = true;
+        Assertions.assertEquals(acres, exres);
+    }
+    @Test
+    void isSearchTree3() {
+        Integer[] arr = {5, 2, 6, 1, 4, null, 8, null, null, 3, null, null, null, 7, 9};
+        Tree<Integer> t  = buildTree(arr, 0);
+        boolean acres = worksheet2.isSearchTree(t);
+//        Integer[] exarr = {5, 1, 4};
+        boolean exres = true;
+        Assertions.assertEquals(acres, exres);
+    }
+    // ---------------------------printDescending---------------------
+    @Test
+    void printDescending1() {
+        Integer[] arr = null;
+        Tree<Integer> t  = buildTree(arr, 0);
+        worksheet2.printDescending(t);
+        List<Integer> acres = new List();
+        Integer[] exarr = {};
+        List<Integer> exres = buildList(exarr);
+        Assertions.assertEquals(acres, exres);
+    }
+    @Test
+    void printDescending2() {
+//        Integer[] arr = {5, 6, 2};
+//        Integer[] arr = {5, 6, 6};
+        Integer[] arr = {5, 2, 6};
+        Tree<Integer> t  = buildTree(arr, 0);
+        worksheet2.printDescending(t);
+        Integer[] acarr = {6, 5, 2};
+        List<Integer> acres = buildList(acarr);
+        Integer[] exarr = {6, 5, 2};
+        List<Integer> exres = buildList(exarr);
+//        boolean exres = true;
+        Assertions.assertEquals(acres, exres);
+    }
+    @Test
+    void printDescending3() {
+        Integer[] arr = {5, 2, 6, 1, 4, null, 8, null, null, 3, null, null, null, 7, 9};
+        Tree<Integer> t  = buildTree(arr, 0);
+        boolean acres = worksheet2.isSearchTree(t);
+//        Integer[] exarr = {5, 1, 4};
+        boolean exres = true;
+        Assertions.assertEquals(acres, exres);
+    }
+    // ---------------------------max---------------------
+    @Test
+    void max1() {
+        Integer[] arr = null;
+        Tree<Integer> t  = buildTree(arr, 0);
+        int acres = worksheet2.max(t);
+        int exres = 0;
+        Assertions.assertEquals(acres, exres);
+    }
+    @Test
+    void max2() {
+//        Integer[] arr = {5, 6, 2};
+//        Integer[] arr = {5, 6, 6};
+        Integer[] arr = {5, 2, 6};
+        Tree<Integer> t  = buildTree(arr, 0);
+        int acres = worksheet2.max(t);
+        int exres = 6;
+//        boolean exres = true;
+        Assertions.assertEquals(acres, exres);
+    }
+    @Test
+    void max3() {
+        Integer[] arr = {5, 2, 6, 1, 4, null, 8, null, null, 3, null, null, null, 7, 9};
+        Tree<Integer> t  = buildTree(arr, 0);
+        int acres = worksheet2.max(t);
+//        Integer[] exarr = {5, 1, 4};
+        int exres = 9;
+        Assertions.assertEquals(acres, exres);
+    }
+    // ---------------------------delete---------------------
+    @Test
+    void delete1() {
+        Integer[] arr = null;
+        Tree<Integer> t  = buildTree(arr, 0);
+        int acres = worksheet2.max(t);
+        int exres = 0;
+        Assertions.assertEquals(acres, exres);
+    }
+    @Test
+    void delete2() {
+//        Integer[] arr = {5, 6, 2};
+//        Integer[] arr = {5, 6, 6};
+        Integer[] arr = {5, 2, 6};
+        Tree<Integer> t  = buildTree(arr, 0);
+        int acres = worksheet2.max(t);
+        int exres = 6;
+//        boolean exres = true;
+        Assertions.assertEquals(acres, exres);
+    }
+    @Test
+    void delete3() {
+        Integer[] arr = {5, 2, 6, 1, 4, null, 8, null, null, 3, null, null, null, 7, 9};
+        Tree<Integer> t  = buildTree(arr, 0);
+        int acres = worksheet2.max(t);
+//        Integer[] exarr = {5, 1, 4};
+        int exres = 9;
+        Assertions.assertEquals(acres, exres);
+    }
+
+
+
+}
