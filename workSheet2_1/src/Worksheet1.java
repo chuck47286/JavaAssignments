47e8f9497ae481418ebd7a676bd8fd908c77ba1c
chuck19-20 <chengyu920420@163.com>
2020-01-28 11:33:19 +0000

    2020-01-28 from the dell.

--- workSheet2_1/src/Worksheet1.java
+++ workSheet2_1/src/Worksheet1.java
@@ -14,11 +14,23 @@
 	}
 	// Exercise 1
 
+	/**
+	 *
+	 * @param m The base of the power function.
+	 * @param n The exponent of the function that is how many times should be multiplied by the base.
+	 * @return The result of the power function.
+	 */
 	 static int power(int m, int n) {
 	 	if (n <= 0) return 1;
 		return m * power(m, n - 1); // replace this by your code
 	}
 
+	/**
+	 *
+	 * @param m The base of the power function.
+	 * @param n The exponent of the function that is how many times should be multiplied by the base.
+	 * @return The result of the fastpower function.
+	 */
 	 static int fastPower(int m, int n) {
 		if (n == 0) return 1;
 		if (n == 1) return m;
@@ -30,6 +42,11 @@
 
 	// Exercise 2
 
+	/**
+	 *
+	 * @param a The list contains the value of type of integer.
+	 * @return The negate value of the original node in the List.
+	 */
 	 static List<Integer> negateAll(List<Integer> a) {
 //		List<Integer> res = new List<>();
 //		Integer aHead = a.getHead();
@@ -42,6 +59,13 @@
 	}
 
 	// Exercise 3
+
+	/**
+	 *
+	 * @param x The target number which is to search for.
+	 * @param a The given the list.
+	 * @return The index of the target number in the list or IllegalArgumentException if is not found.
+	 */
 	 static int find(int x, List<Integer> a) {
 		if (a.isEmpty()) {
 			throw new IllegalArgumentException("cannot find");
@@ -55,6 +79,11 @@
 
 	// Exercise 4
 
+	/**
+	 *
+	 * @param a The given list.
+	 * @return whether the value of node is even in the list.
+	 */
 	 static boolean allEven(List<Integer> a) {
 		if (a.isEmpty())
 			return true;
@@ -63,6 +92,11 @@
 
 	// Exercise 5
 
+	/**
+	 *
+	 * @param a Given a list of integer.
+	 * @return a new list which contains all the even element in the given list.
+	 */
 	 static List<Integer> evenNumbers(List<Integer> a) {
 		if (a.isEmpty())
 			return a;
@@ -74,6 +108,12 @@
 
 	// Exercise 6
 
+	/**
+	 *
+	 * @param a Given a list of integers.
+	 * @return a boolean value indicating whether a is sorted in
+	 * descending order.
+	 */
 	 static boolean sorted(List<Integer> a) {
 		if (a.isEmpty() || a.getTail().isEmpty()) return true;
 		return a.getHead() >= a.getTail().getHead() && sorted(a.getTail()); // replace this by your code
@@ -81,6 +121,12 @@
 
 	// Exercise 7
 
+	/**
+	 *
+	 * @param a Given a list of integer.
+	 * @param b Given a list of integer.
+	 * @return The list of integer which element arranged in a descending order.
+	 */
 	 static List<Integer> merge(List<Integer> a, List<Integer> b) {
 		if (a.isEmpty()) return b;
 		if (b.isEmpty()) return a;
@@ -91,6 +137,11 @@
 
 	// Exercise 8
 
+	/**
+	 *
+	 * @param a Given a list of integers.
+	 * @return The list of integers which remove the duplicate element from the given list.
+	 */
 	static List<Integer> removeDuplicates(List<Integer> a) {
 		if (a.isEmpty() || a.getTail().isEmpty()) return a;
 		List<Integer> lista = isNotDuplicate(a);
